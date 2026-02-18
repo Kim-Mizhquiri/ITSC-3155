482758a8f7702402c2d624ffa32248034389f122
Kim Mizhquiri <kmizhqui@charlotte.edu>
2026-02-17 22:40:01 -0500

    Update Cashier.py

--- Assignment 2/cashier.py
+++ Assignment 2/cashier.py
@@ -3,11 +3,24 @@ class Cashier:
         pass
 
     def process_coins(self):
-        """Returns the total calculated from coins inserted.
-           Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        """Returns the total calculated from coins inserted."""
+
+        print("Please insert coins")
+
+        large_dollar = input("How many large dollars?: ")
+        half_dollar = input("How many half dollars?: ")
+        quarters = input("How many quarters?: ")
+        dimes = input("How many dimes?: ")
+        nickels = input("How many nickels?: ")
+
+        # total monetary value
+        coins = float(large_dollar) + (float(half_dollar) * 0.50) + (float(quarters) * 0.25) + (float(dimes) * 0.10) + (float(nickels) * 0.05)
+
+        return coins
 
     def transaction_result(self, coins, cost):
-        """Return True when the payment is accepted, or False if money is insufficient.
-           Hint: use the output of process_coins() function for cost input"""
-        ##
+        """Return True when the payment is accepted, or False if money is insufficient."""
+        if coins < cost:
+            return False
+        else:
+            return True
