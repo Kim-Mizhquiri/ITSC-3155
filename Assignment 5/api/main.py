2b5b2f612d0854e12c7657f8318c0fa07d9f085e
Kim Mizhquiri <kmizhqui@charlotte.edu>
2026-04-03 13:09:16 -0400

    feat: add recipes API endpoints

--- Assignment 5/api/main.py
+++ Assignment 5/api/main.py
@@ -7,6 +7,7 @@ from .controllers import orders
 from .dependencies.database import engine, get_db
 
 from .controllers import order_details
+from .controllers import recipes
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -95,3 +96,34 @@ def delete_one_order_detail(order_detail_id: int, db: Session = Depends(get_db))
     if order_detail is None:
         raise HTTPException(status_code=404, detail="Order detail not found")
     return order_details.delete(db=db, order_detail_id=order_detail_id)
+
+# RECIPES CONTROLLER ENDPOINTS
+
+@app.post("/recipes/", response_model=schemas.Recipe, tags=["Recipes"])
+def create_recipe(recipe: schemas.RecipeCreate, db: Session = Depends(get_db)):
+    return recipes.create(db=db, recipe=recipe)
+
+@app.get("/recipes/", response_model=list[schemas.Recipe], tags=["Recipes"])
+def read_recipes(db: Session = Depends(get_db)):
+    return recipes.read_all(db)
+
+@app.get("/recipes/{recipe_id}", response_model=schemas.Recipe, tags=["Recipes"])
+def read_one_recipe(recipe_id: int, db: Session = Depends(get_db)):
+    recipe = recipes.read_one(db, recipe_id=recipe_id)
+    if recipe is None:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+    return recipe
+
+@app.put("/recipes/{recipe_id}", response_model=schemas.Recipe, tags=["Recipes"])
+def update_one_recipe(recipe_id: int, recipe: schemas.RecipeUpdate, db: Session = Depends(get_db)):
+    recipe_db = recipes.read_one(db, recipe_id=recipe_id)
+    if recipe_db is None:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+    return recipes.update(db=db, recipe=recipe, recipe_id=recipe_id)
+
+@app.delete("/recipes/{recipe_id}", tags=["Recipes"])
+def delete_one_recipe(recipe_id: int, db: Session = Depends(get_db)):
+    recipe = recipes.read_one(db, recipe_id=recipe_id)
+    if recipe is None:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+    return recipes.delete(db=db, recipe_id=recipe_id)
